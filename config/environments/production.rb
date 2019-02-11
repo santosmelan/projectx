0544037d38f236f9681a2f048bde0b734de1f6fb
Melanie Santos <santosmelan@gmail.com>
2019-02-11 10:11:41 +0800

    edit email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "projectx_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'santosmelan-projectx.herokuapp.com'
-  
+
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
