013fa9bdfdd68dc0d9c530c9e108712a12aaad92
Melanie Santos <santosmelan@gmail.com>
2019-02-12 09:55:02 +0800

    edit mail settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,11 +24,11 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  #config.mailer_sender = 'no-reply@projectx.com'
-  config.mailer_sender = "mail-to-send@from.com"
+  config.mailer_sender = 'no-reply@projectx.com'
+  #config.mailer_sender = "mail-to-send@from.com"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
